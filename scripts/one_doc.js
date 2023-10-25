36d4581594aafdb6ce0725defa36683a01c3167f
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-10-25 22:31:28 +0300

    25.10

--- scripts/one_doc.js
+++ scripts/one_doc.js
@@ -1,4 +1,21 @@
+const GetUrlParam=()=>{
+    let urlParams = new URLSearchParams(window.location.search);
+    let VALUE = urlParams.get('name')
+    return VALUE
+}
+
+const ChangeMainProfile=(doc_name)=>{
+    if(doc_name=="Мицько"){
+        console.log("1")
+    }
+    else if(doc_name=="Саковець"){
+        console.log("2")
+    }
+    else if(doc_name=="Проценко"){
+        console.log("3")
+    } 
+}
 
 document.addEventListener("DOMContentLoaded",()=>{
-    console.log($(location).attr('href'))
+    ChangeMainProfile(GetUrlParam())
 })
