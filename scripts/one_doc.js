44caecfb8b9ac99765ec156885dc8a09a1e21da5
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-10-26 12:51:19 +0300

    26.10

--- scripts/one_doc.js
+++ scripts/one_doc.js
@@ -5,14 +5,27 @@ const GetUrlParam=()=>{
 }
 
 const ChangeMainProfile=(doc_name)=>{
+    const PICTURE = $(".profile__picture")
+    const NAME = $(".profile__info_name")
+    const PROFESSION = $(".profile__info_profession")
+    const ROOM = $(".profile__info_text_changable.room")
+    const CATEGORY = $(".profile__info_text_changable.category")
+    const EDUCATION = $(".profile__info_text_changable.education")
+    const EXPIRIENCE = $(".profile__info_text_changable.experience")
     if(doc_name=="Мицько"){
-        console.log("1")
+        PICTURE.attr("src","imgs/5eda08da7492ea6fcde0aec01e5d26ea 1.png");
+        NAME.text("Мицько Мар’яна");
+        PROFESSION.text("Лікар-невропатолог");
     }
     else if(doc_name=="Саковець"){
-        console.log("2")
+        PICTURE.attr("src","imgs/808458c0f9254b3df8e561de9b851ac9 1.png");
+        NAME.text("Саковець Оксана");
+        PROFESSION.text("Лікар-гастроентеролог");
     }
     else if(doc_name=="Проценко"){
-        console.log("3")
+        PICTURE.attr("src","imgs/666e05e3f240f2e03dad7ab309097eb4 1.png");
+        NAME.text("Проценко Олексій");
+        PROFESSION.text("Лікар-уролог");
     } 
 }
 
