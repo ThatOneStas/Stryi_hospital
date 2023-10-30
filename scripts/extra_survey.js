79cab963c34a43aec20b13e93443decdd1e737c9
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-10-30 12:10:20 +0200

    30.10

---
+++ scripts/extra_survey.js
@@ -0,0 +1,24 @@
+let rate = 1
+
+const changeStars = (number)=>{
+    if(number==1){
+        rate = 1
+    }
+    else if(number==2){
+        rate = 2
+    }
+    else if(number==3){
+        rate = 3
+    }
+    else if(number==4){
+        rate = 4    
+    }
+    else if(number==5){
+        rate = 5    
+    }
+    console.log(rate)
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+})
